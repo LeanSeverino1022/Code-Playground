002edf2338bd977705c53971bf38373fa8261259
Puyih Severino <yihseverino@gmail.com>
2017-06-07 14:58:32 +0800

    finished TTH challenge
    added rooms.json
    added styling for roomsList - main.css
    added new section for roomsList = index.html
    widget.js - check if room is available or occupied

--- Javascript/AJAX basics/jQuery_and_AJAX_2/js/widget.js
+++ Javascript/AJAX basics/jQuery_and_AJAX_2/js/widget.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
 
     var url = "data/employees.json";
 
-    // $.getJSON(url, data, callback);
-
-    // we don't have any data to send so lets take out the data argument
+    // $.getJSON(url, data, callback); : we don't have any data to send so lets take out the data argument
     $.getJSON(url, function(response) {
         var statusHTML = '<ul class="bulleted">'; 
 
@@ -23,6 +21,28 @@ $(document).ready(function(){
         statusHTML += '</ul>';
         $('#employeeList').html(statusHTML);
     }); //end getJSON
+
+    $.getJSON('data/rooms.json', function( dat ) {
+    
+        var roomHTML  ='<ul class="rooms">';
+        
+        $.each(dat, function(key, val) {
+        
+        if(val.available === true ){
+            roomHTML += '<li class="empty">' + val.room + '</li>';
+        }
+        else {
+            roomHTML += '<li class="full">' + val.room + '</li>';
+        }
+        
+        }); //end each
+        
+        roomHTML += '</ul>';
+        document.getElementById("roomList").innerHTML = roomHTML;
+    
+  
+    });// end getJSON
+  
 
 }); /*end ready*/
 
