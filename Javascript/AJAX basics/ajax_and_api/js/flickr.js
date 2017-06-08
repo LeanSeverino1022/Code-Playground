11a402591e9756a36b504bb0ad2ea8defd92bb1c
Puyih Severino <yihseverino@gmail.com>
2017-06-08 17:03:39 +0800

    AJAX and API: calling the jquery,getjson method
    added code to:
    make the JSONP request
    setup data to pass to API
    create a callback function

---
+++ Javascript/AJAX basics/ajax_and_api/js/flickr.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $('button').click( function() {
+        $('button').removeClass("selected");
+        $(this).addClass("selected");    
+
+        flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"; 
+        var animal = $(this).text();
+        var flickrOptions = {
+            tags: animal,
+            format: "json",
+        };
+
+        function displayPhotos(data) {
+            
+        }
+
+        $.getJSON(flickrAPI, flickrOptions,displayPhotos);
+
+    });
+});//end ready
