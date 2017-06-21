4653c8148c20b163a9aee29be527594465f5f237
Puyih Severino <yihseverino@gmail.com>
2017-06-21 12:11:17 +0800

    playlist proj; creating object and instantiating
    app.js
    - create a new playlist obj and 2 song objects. adds the songs to the
    playlist

---
+++ Javascript/OOP JS/playlist_proj/app.js
@@ -0,0 +1,7 @@
+var playlist = new Playlist();
+
+var Buloy = new Song("Buloy", "Parokya", "3:00");
+var Buloy2 = new Song("Buloy2", "Parokya2", "3:00");
+
+playlist.add(Buloy);
+playlist.add(Buloy2);
--- Javascript/OOP JS/playlist_proj/playlist.js
+++ Javascript/OOP JS/playlist_proj/playlist.js
@@ -1,21 +1,32 @@
 function Playlist() {
-
+    // when we create a playlist, we 1st create an empty array of songs 
+    this.songs = [];
+    this.nowPlaylingIndex = 0;
 }
 
-Playlist.prototype.add = function() {
-
+Playlist.prototype.add = function(song) {
+    this.songs.push(song);
 };
 
 Playlist.prototype.play = function() {
-
+    var currentSong = this.songs[this.nowPlaylingIndex];
+    // console.log("Playing song index # " + currentSong);
+    currentSong.play();
 };
 
 Playlist.prototype.stop = function(){
-
+    var currentSong = this.songs[this.nowPlaylingIndex];
+    currentSong.stop();
 };
 
 Playlist.prototype.next = function() {
+    this.stop();
+    this.nowPlaylingIndex++;
 
+    if(this.nowPlaylingIndex === this.songs.lengh){
+        this.nowPlaylingIndex = 0;
+    }
+    this.play();
 };
 
 Playlist.prototype.renderIn = function() {
