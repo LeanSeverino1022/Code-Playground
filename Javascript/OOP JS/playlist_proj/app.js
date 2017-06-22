b678460ec0567a9eb94c16dfaebd835f1bace146
Puyih Severino <yihseverino@gmail.com>
2017-06-23 06:52:27 +0800

    UI buttons now does something

--- Javascript/OOP JS/playlist_proj/app.js
+++ Javascript/OOP JS/playlist_proj/app.js
@@ -9,3 +9,21 @@ playlist.add(Buloy2);
 var playlistElement = document.getElementById('playlist');
 
 playlist.renderInElement(playlistElement);
+
+var playButton = document.getElementById("play");
+playButton.addEventListener("click", function(){
+    playlist.play();
+    playlist.renderInElement(playlistElement);
+});
+
+var nextButton = document.getElementById("next");
+nextButton.addEventListener("click", function(){
+    playlist.next();
+    playlist.renderInElement(playlistElement);
+});
+
+var stopButton = document.getElementById("stop");
+stopButton.addEventListener("click", function(){
+    playlist.stop();
+    playlist.renderInElement(playlistElement);
+});
